f2dde8de010fd04f6fd50d694473c7e10913ba49
rapuncia <keruzu@mail.ru>
2020-05-26 15:26:04 +0300

    authenticated request

--- FeatureContext.php
+++ FeatureContext.php
@@ -10,6 +10,9 @@ use Behat\Gherkin\Node\TableNode;
 class FeatureContext implements Context
 {
     protected $response = null;
+    protected $username = null;
+    protected $password = null;
+    protected $client   = null;
 
     /**
      * Initializes context.
@@ -17,9 +20,12 @@ class FeatureContext implements Context
      * Every scenario gets its own context instance.
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
+     * @param array $parameters
      */
-    public function __construct()
+    public function __construct($github_username, $github_password)
     {
+        $this->username = $github_username;
+        $this->password = $github_password;
     }
 
     /**
@@ -40,18 +46,58 @@ class FeatureContext implements Context
     }
 
     /**
-     * @Then I get a result
+     * @Then I expect a :arg1 response code
      */
-    public function iGetAResult()
+    public function iExpectAResponseCode($arg1)
     {
         $response_code = $this->response->getStatusCode();
-        if ($response_code <> 200) {
-            throw new Exception("It didn't work. We expected a 200 response code but got a " . $response_code);
+        if ($response_code <> $arg1) {
+            throw new Exception("It didn't work. We expected a $arg1 response code but got a " . $response_code);
         }
+    }
 
+    /**
+     * @Then I expect at least :arg1 result
+     */
+    public function iExpectAtLeastResult($arg1)
+    {
         $data = json_decode($this->response->getBody(), true);
-        if ($data['total_count'] == 0) {
-            throw new Exception("We found zero results!");
+        if ($data['total_count'] < $arg1) {
+            throw new Exception("We expected at least $arg1 results but found: " . $data['total_count']);
         }
+    }
+
+    /**
+     * @Given I am an authenticated user
+     */
+    public function iAmAnAuthenticatedUser()
+    {
+        $client = new GuzzleHttp\Client(
+            [
+                'base_uri' => 'https://api.github.com',
+                'auth' => [$this->username, $this->password]
+            ]
+        );
+        $response = $client->get('/');
+
+        if (200 != $response->getStatusCode()) {
+            throw new Exception("Authentication didn't work!");
+        }
+    }
+
+    /**
+     * @When I request a list of my repositories
+     */
+    public function iRequestAListOfMyRepositories()
+    {
+        throw new Exception();
+    }
+
+    /**
+     * @Then The results should include a repostory name :arg1
+     */
+    public function theResultsShouldIncludeARepostoryName($arg1)
+    {
+        throw new Exception();
     }
 }
